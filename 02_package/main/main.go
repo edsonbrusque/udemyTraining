e8f170f38003198962db26548c397dc4d26d3d57
edsonbrusque <edsonbrusque@gmail.com>
2017-04-27 17:54:06 -0300

    Section 2!

---
+++ 02_package/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/edsonbrusque/udemyTraining/02_package/stringutil"
+	"github.com/edsonbrusque/udemyTraining/02_package/icomefromalaska"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
