ae5e60f1f945923baad855d6a77234291a6116b3
edsonbrusque <edsonbrusque@gmail.com>
2017-05-23 17:28:18 -0300

    20170523

--- 22_go-routines/12_channels_pipeline/02_sq-output/main.go
+++ 22_go-routines/12_channels_pipeline/02_sq-output/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	// Set up the pipeline and consume the output.
-	for n:= range sq(sq(gen(2, 3))) {
+	for n := range sq(sq(gen(2, 3))) {
 		fmt.Println(n)
 	}
 }
@@ -21,7 +21,7 @@ func gen(nums ...int) <-chan int {
 }
 
 func sq(in <-chan int) <-chan int {
-	out := make (chan int)
+	out := make(chan int)
 	go func() {
 		for n := range in {
 			out <- n * n
--- 22_go-routines/12_channels_pipeline/04_challenge-solution/main.go
+++ 22_go-routines/12_channels_pipeline/04_challenge-solution/main.go
@@ -3,36 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	var c []chan int
-	for i := 0; i < 100; i++ {
-		c = append(c, factorial(i))
-	}
+	f := factorial(gen())
 
-	for n := range c {
+	for n := range f {
 		fmt.Println(n)
 	}
 }
 
-func factorial(n int) chan int {
+func gen() <-chan int {
 	out := make(chan int)
 	go func() {
-		total := 1
-		for i := n; i > 0; i-- {
-			total *= i
+		for i := 0; i < 10; i++ {
+			for j := 3; j < 13; j++ {
+				out <- j
+			}
 		}
-		out <- total
 		close(out)
 	}()
 	return out
 }
 
-/*
-CHALLENGE #1:
--- Change the code above to execute 100 factorial computations concurrently and in parallel
--- Use the "pipeline" pattern to accomplish this
-
-POST TO DISCUSSION:
--- What realizations did you have about working with concurrent code when building your solution?
--- eg, what insights did you have wich helped you understand working with concurrency?
--- Post your answer, and read other answers, here: https:/goo.gl/uJa99G
- */
+func factorial(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			total := 1
+			for i := n; i > 0; i-- {
+				total *= i
+			}
+			out <- total
+		}
+		close(out)
+	}()
+	return out
+}
